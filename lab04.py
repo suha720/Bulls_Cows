88173e29d3de3c3628bfc2e5f7cb9e381b07074f
suha <suha720@naver.com>
2022-01-05 14:06:59 +0900

    Create lab04.py and README.md

---
+++ lab04.py
@@ -0,0 +1,58 @@
+#코드잇, 숫자 야구 프로젝트
+from random import randint
+
+
+def generate_numbers():
+    numbers = []
+
+    while len(numbers) < 3:
+        new_number = randint(0, 9)
+        if new_number not in numbers:
+            numbers.append(new_number)
+
+    return numbers
+
+
+def take_guess():
+    new_guess = []
+    while len(new_guess) < 3:
+        num = int(input("{}번째 수를 입력하세요: ".format(len(new_guess) + 1)))
+
+        if num < 0 or num > 9:
+            print("0에서 9까지의 수를 입력해 주세요!")
+        elif num in new_guess:
+            print("중복되는 숫자입니다. 다시 입력하세요.")
+        else:
+            new_guess.append(num)
+
+    return new_guess
+
+
+def get_score(guesses, solution):
+    strike_count = 0
+    ball_count = 0
+
+    for i in range(3):
+        if guesses[i] == solution[i]:
+            strike_count += 1
+        elif guesses[i] in solution:
+            ball_count += 1
+
+    return strike_count, ball_count
+
+
+# 여기서부터 게임 시작!
+ANSWER = generate_numbers()
+tries = 0
+
+while True:
+    user_guess = take_guess()
+    s, b = get_score(user_guess, ANSWER)
+
+    print("{}S {}B\n".format(s, b))
+    tries += 1
+
+    if s == 3:
+        break
+
+print("축하합니다. {}번 만에 세 숫자의 값과 위치를 모두 맞추셨습니다.".format(tries))
